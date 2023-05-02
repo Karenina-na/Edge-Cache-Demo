069a1795b34066b1b8e823eed97b37a622b15fa2
Karenina-na <weizixiang0@outlook.com>
2023-05-02 17:37:09 +0800

    对每个请求加入时延属性

--- Main/Env/ProbAndReward/Request.py
+++ Main/Env/ProbAndReward/Request.py
@@ -1,30 +1,30 @@
 import numpy as np
-from probability import ProbabilityDensity, ProbabilityMass
+from Main.Env.ProbAndReward.probability import ProbabilityDensity, ProbabilityMass
 
 
 class Request:
-    def __init__(self, observation_space, request_number):
-        self.observation_space = observation_space
+    def __init__(self, state_space, request_number):
+        self.state_space = state_space
         self.request_number = request_number
         self.request = []
         self.time_out = []
 
         # self.mu = 1.2
         # self.sigma = 1
-        self.class_sigma = 1
+        self.class_sigma = 10
         self.lam = 2.2
         self.a = 0.6
         # 时延范围
         self.time_out_range = [10, 10000]
 
     def RequestCreate(self):
-        distribution = ProbabilityDensity.Zipf(np.arange(len(self.observation_space)),
-                                               self.a, len(self.observation_space))
+        distribution = ProbabilityDensity.Zipf(np.arange(len(self.state_space)),
+                                               self.a, len(self.state_space))
         distribution = distribution / sum(distribution)
         # 按照概率分布生成n个请求
         requests = []
         while len(requests) < self.request_number:
-            index = np.random.choice(np.arange(len(self.observation_space)), p=distribution)
+            index = np.random.choice(np.arange(len(self.state_space)), p=distribution)
             requests.append(index)
         self.request = requests
 
@@ -32,13 +32,21 @@ class Request:
         requests_time_out = []
         for i in range(len(self.request)):
             time_mu = ProbabilityMass.Poisson(self.request[i], self.lam)
-            error_dis = ProbabilityDensity.Normal(np.arange(len(self.observation_space)), time_mu, self.class_sigma)
-            error_dis = error_dis / sum(error_dis)
-            time_percent = np.random.choice(np.arange(len(self.observation_space)), p=error_dis)
-            # time_percent = ProbabilityDensity.Normal(self.request[i], self.mu, self.sigma)
-            # 归一化到[10, 10000]间
-            time_out = time_percent * (self.time_out_range[1] - self.time_out_range[0]) + self.time_out_range[0]
-            requests_time_out.append(time_out)
+
+            # 归一化到[0, self.time_out_range[1]]间
+            time_mu = time_mu * (self.time_out_range[1] - self.time_out_range[0])
+            time_mu = int(np.round(time_mu))
+
+            # 时延误差概率分布
+            time_error = ProbabilityDensity.Normal(np.arange(self.time_out_range[1] - self.time_out_range[0]),
+                                                   time_mu, self.class_sigma)
+            time_error = time_error / sum(time_error)
+
+            # 时延误差
+            time_out = np.random.choice(np.arange(self.time_out_range[1] - self.time_out_range[0]), p=time_error)
+
+            # 归一化到[self.time_out_range[0], self.time_out_range[1]]间
+            requests_time_out.append(time_out + self.time_out_range[0])
         self.time_out = requests_time_out
 
 
@@ -63,16 +71,16 @@ if __name__ == '__main__':
     for i in range(len(states)):
         count[i] = index.count(i)
     plt.subplot(413)
-    plt.bar(range(len(request.observation_space)), count, color='g')
+    plt.bar(range(len(request.state_space)), count, color='g')
 
     # 统计时延的分布
     requests_time_out = []
-    for i in range(len(request.observation_space)):
+    for i in range(len(request.state_space)):
         time_percent = ProbabilityMass.Poisson(i, request.lam)
         # 归一化到[10, 10000]间
         time_out = time_percent * (request.time_out_range[1] - request.time_out_range[0]) + request.time_out_range[0]
         requests_time_out.append(time_out)
     plt.subplot(414)
-    plt.bar(range(len(request.observation_space)), requests_time_out, color='y')
+    plt.bar(range(len(request.state_space)), requests_time_out, color='y')
 
     plt.show()
--- Main/Env/env.py
+++ Main/Env/env.py
@@ -1,45 +1,50 @@
 import gym
 from Main.Env.ProbAndReward.probability import ProbabilityDensity
 import numpy as np
+from Main.Env.ProbAndReward.Request import Request
 
 
 class Env(gym.Env):
-    def __init__(self, S_dim, a_dim, a, request_number, stop_number):
-        self.observation_space = np.ones(S_dim)
+    def __init__(self, S_dim, a_dim, request_number, stop_number):
+        self.observation_space = np.zeros(shape=(2, S_dim))
         self.action_space = np.arange(a_dim)
-        self.a = a
-        self.request = []
-        self.request_number = request_number
+        self.request = Request(range(S_dim), request_number)
+        self.request_number = request_number  # 每次生成的请求数量
         self.cache = 0
         self.total = 0
-        self.stop_number = stop_number
+        self.stop_number = stop_number  # 仿真step步数
+        self.request_time_out_dis = [[] for _ in range(S_dim)]  # 时延分布
 
     def step(self, action):
-
         # 计算缓存命中率，即奖励
         reward = 0
-        for req in self.request:
+        for req in self.request.request:
             if req in action:
                 reward += 1
                 self.cache += 1
             else:
                 reward += -1
             self.total += 1
-        # 消除第一个维度
-        self.observation_space = np.squeeze(action)
-
         # 生成新的请求
-        # -----------------------------------------------------------------------------distribution#
-        distribution = ProbabilityDensity.Zipf(np.arange(len(self.observation_space)),
-                                               self.a, len(self.observation_space))
-        # 归一化
-        distribution = distribution / sum(distribution)
-        # 按照概率分布生成n个请求
-        requests = []
-        while len(requests) <= self.request_number:
-            index = np.random.choice(np.arange(len(self.observation_space)), p=distribution)
-            requests.append(index)
-        self.request = requests
+        self.request.RequestCreate()
+        self.request.RequestTimeOut()
+
+        # observation_space更新 [频率，时延均值] [2, S_dim]
+        frequency = np.zeros(shape=(len(self.observation_space[0])))
+        time_out = np.zeros(shape=(len(self.observation_space[1])))
+        for i in range(len(self.observation_space[0])):
+            frequency[i] += self.request.request.count(i)
+        for index in self.request.request:
+            time_out[index] += self.request.time_out[index]
+        self.observation_space[0] = frequency
+        self.observation_space[1] = time_out / self.request_number
+
+        # request_time_out_dis更新 [状态index, 时延]  [S_dim, S_dim]
+        print(self.request_time_out_dis)
+        exit(0)
+        for index in self.request.request:
+            self.request_time_out_dis[index].append(self.request.time_out[index])
+
 
         # 结束条件
         if self.total >= self.stop_number:
@@ -47,23 +52,60 @@ class Env(gym.Env):
         return self.observation_space, reward, False, False, False
 
     def reset(self):
-        # -----------------------------------------------------------------------------distribution#
-        distribution = ProbabilityDensity.Zipf(np.arange(len(self.observation_space)),
-                                               self.a, len(self.observation_space))
-        # 归一化
-        distribution = distribution / sum(distribution)
-        # 按照概率分布生成n个请求
-        requests = []
-        while len(requests) <= self.request_number:
-            index = np.random.choice(np.arange(len(self.observation_space)), p=distribution)
-            requests.append(index)
-        self.request = requests
+        # 生成新的请求
+        self.request.RequestCreate()
+        self.request.RequestTimeOut()
+
+        # observation_space更新
+        frequency = np.zeros(shape=(len(self.observation_space[0])))
+        for i in range(len(self.observation_space[0])):
+            frequency[i] += self.request.request.count(i)
+        time_out = np.zeros(shape=(len(self.observation_space[1])))
+        for index in self.request.request:
+            time_out[index] += self.request.time_out[index]
+        self.observation_space[0] = frequency
+        self.observation_space[1] = time_out / self.request_number
+
+        # request_time_out_dis更新 [状态index, 时延]  [S_dim, S_dim]
+        for index in self.request.request:
+            self.request_time_out_dis[index].append(self.request.time_out[index])
+
         self.cache = 0
         self.total = 0
         return self.observation_space, False
 
-    def render(self):
-        pass
 
-    def close(self):
-        pass
+if __name__ == "__main__":
+    s_dim = 10
+    a_dim = 5
+    request_number = 50
+    stop_number = 100
+    env = Env(s_dim, a_dim, request_number, stop_number)
+    count = np.zeros(shape=s_dim)
+    time_out = np.zeros(shape=s_dim)
+    time_out_dis = np.zeros(shape=(s_dim, s_dim))
+    env.reset()
+    while True:
+        # observation_space [频率，时延均值] [2, S_dim]
+        observation_space, _, done, _, _ = env.step(np.arange(a_dim))
+        request_error_dis = env.request_time_out_dis
+        print(request_error_dis)
+        exit(0)
+        for i in range(s_dim):
+            count[i] += observation_space[0][i]
+            time_out[i] += observation_space[1][i]
+            time_out[i] /= 2  # 时延均值
+
+        if done:
+            break
+    import matplotlib.pyplot as plt
+
+    plt.figure(figsize=(10, s_dim * 3), dpi=200)
+    plt.subplots_adjust(hspace=0.5)
+    for i in range(s_dim):
+        plt.subplot(s_dim, 1, i + 1)
+        plt.plot(range(s_dim), count[i], '-', color='r')
+        plt.ylabel('count')
+        plt.xlabel('step')
+        plt.title('state %d' % i)
+    plt.show()
